8b6c7bf15d740c65b23792b29c6259b67aeef5a3
KillSwitch999 <140569515+KillSwitch999@users.noreply.github.com>
2025-07-10 16:29:21 -0400

    Initial Commit

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace FactoryPattern
+﻿using System;
+namespace FactoryPattern
 {
-    public class Program
-    {
-        static void Main(string[] args)
+        class Program
         {
-            Console.WriteLine("Hello, World!");
+            static void Main(string[] args)
+            {
+                Console.WriteLine("How many tires does your vehicle have?");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out int tireCount))
+                {
+                    IVehicle vehicle = VehicleFactory.GetVehicle(tireCount);
+                    vehicle?.Drive();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                }
+            }
         }
-    }
+
+        public interface IVehicle
+        {
+            void Drive();
+        }
+
+        public class Car : IVehicle
+        {
+            public void Drive()
+            {
+                Console.WriteLine("Building a new Car!");
+            }
+        }
+
+        public class Motorcycle : IVehicle
+        {
+            public void Drive()
+            {
+                Console.WriteLine("Building a new Motorcycle!");
+            }
+        }
+
+        public class VehicleFactory
+        {
+            public static IVehicle GetVehicle(int tireCount)
+            {
+                return tireCount switch
+                {
+                    2 => new Motorcycle(),
+                    4 => new Car(),
+                    _ => null
+                };
+            }
+        }
+
 }
